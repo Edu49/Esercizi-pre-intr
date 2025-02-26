582c8c1afa832d6617e2c7ec104253fcd9e61215
Edu49 <eduardgabriel.donisan@iismajoranamoncalieri.edu.it>
2025-02-26 11:59:32 +0000

    Esercizi

---
+++ interrogazione.py
@@ -0,0 +1,15 @@
+#Creare un range di valori da 1 a 10 compresi
+
+r = range(1, 11)
+
+#Creare una lista di valori interi da 1 a 10
+
+lista = list(range(1, 11))
+print(lista)
+
+numeri = []
+i = 0
+while i < 10:
+    i += 1
+    numeri.append(i)
+print(numeri)
